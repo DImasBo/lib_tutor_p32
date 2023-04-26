4642325412d1663440a146e4593148271e65e5c2
dimasbo <dimasbo.work@gmail.com>
2023-04-26 19:33:59 +0300

    update files about libs

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-# import files_control
-
-# settings, config_admin config_db
-
-# from files_control import read_data, save_data
-# from tools import read_data, save_data
-from tools.files_control import read_data, save_data, FILE_NAME
-# from random import randint
-
-action = input("action: ")
-
-if action == "s":
-    save_data(["test", "itStep", "Dimas"])
-
-if action == "r":
-    l = read_data()
-    print(l)
-
+# text about pip
